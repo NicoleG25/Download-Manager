b59a6a46d3eb8b5386985e1581e847750d0e4526
nicole <nicolegru25@gmail.com>
2020-01-04 15:07:32 +0200

    Nicole - dealing with download percentages

--- Thread.java
+++ Thread.java
@@ -7,22 +7,44 @@ public class Thread extends java.lang.Thread {
     protected int start;
     protected String link;
     protected long ID;
+    protected double buffer; // determines the number of bytes to update the percentage and send
 
-    public Thread(String link,int start,int end) {
+
+    public Thread(String link,int start,int end, double buffer) {
         this.maxRange = end; //max byte to download
         this.start = start;
         this.link = link;
         this.ID = this.getId();
+        this.buffer = buffer;
 
     }
-    // TODO: test
-    public void connect() throws IOException {
-        URL url = new URL(link);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        String byteRange = "bytes=" + this.start + "-" + this.maxRange;
-        urlConnection.setRequestProperty("Range", byteRange); //should handle download range
-        urlConnection.connect();
-        System.out.println("[" + this.ID + "] " + "Start downloading range (" + byteRange +") from:\n" + this.link);
+    // TODO: test + finish implementing errors
+    public void connect() {
+
+
+        try {
+            URL url = new URL(link);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            String byteRange = "bytes=" + this.start + "-" + this.maxRange;
+            urlConnection.setRequestProperty("Range", byteRange); //should handle download range
+            urlConnection.setConnectTimeout(500);
+
+
+            try {
+                urlConnection.connect();
+
+            }
+            catch(SocketTimeoutException e){
+
+            }
+
+        }
+        catch (IOException e) {
+
+        }
+
+
+
 
     }
 
